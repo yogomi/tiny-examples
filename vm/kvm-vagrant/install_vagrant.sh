b7d5794a93f88dce6206d4f921cbe33d668b84dd
Makoto Yano <yan133@gmail.com>
2016-05-27 15:10:28 +0900

    KVMのネストができなかったためdebianのバージョンを調整。
    8.4.0はダメだった。7.3.0で行けた

--- vm/kvm-vagrant/install_vagrant.sh
+++ vm/kvm-vagrant/install_vagrant.sh
@@ -15,4 +15,10 @@ vagrant plugin install vagrant-berkshelf vagrant-libvirt vagrant-mutate vagrant-
 vagrant box add debian-8.4.0-amd64 https://s3.eu-central-1.amazonaws.com/ffuenf-vagrantboxes/debian/debian-8.4.0-amd64_virtualbox.box
 vagrant mutate debian-8.4.0-amd64 --input-provider virtualbox libvirt
 
+vagrant box add debian-7.3.0-amd64 https://dl.dropboxusercontent.com/u/29173892/vagrant-boxes/debian7.3.0-vbox4.3.6-puppet3.4.1.box
+vagrant mutate debian-7.3.0-amd64 --input-provider virtualbox libvirt
+
+vagrant box add centos-7.1-amd64 https://github.com/CommanderK5/packer-centos-template/releases/download/0.7.1/vagrant-centos-7.1.box
+vagrant mutate centos-7.1-amd64 --input-provider virtualbox libvirt
+
 sudo usermod --group libvirt vagrant
